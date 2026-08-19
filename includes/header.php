45142a9357328f778562ef79a59b2b858dd85ae7
Xzaviar <user@MacBookPro.lan>
2026-08-19 19:40:52 -0400

    Update Community Leaders to clean text roster, move Hugh McColl and Susan Rodriguez McDowell photos to photo gallery, set logo as favicon, and increase navbar logo & font size

--- includes/header.php
+++ includes/header.php
@@ -16,6 +16,10 @@ require_once __DIR__ . '/config.php';
   <meta property="og:url" content="https://www.dimpleajmera.com">
   <meta property="og:image" content="assets/images/DimpleMainLogo.png">
 
+  <!-- Favicon -->
+  <link rel="icon" type="image/png" href="assets/images/whiteboard_9_navlogo.png">
+  <link rel="shortcut icon" href="favicon.ico">
+
   <!-- Bootstrap 5 CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <!-- Bootstrap Icons -->
